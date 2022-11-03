0a806d2a415b62cf393023df1ad0a410055b4674
Your Name <wesleyvc2110@gmail.com>
2022-11-03 17:14:29 -0300

    fix: athentication route

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,28 @@
-import express, { NextFunction, request, Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import routes from './routes';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-// import auth from './middleware/verify';
+import auth from './middleware/verify';
 
 const app = express();
 app.use(express.json());
-// app.use(cookieParser());
-// app.use(cors({
-//     origin: '*',
-//     credentials: true,
-//     methods: 'GET, PUT, POST,optionsSuccessStatus, DELETE',
-// }));
+app.use(cookieParser());
+app.use(cors({
+    origin: '*', // url do front
+    credentials: true,
+    methods: 'GET, PUT, POST, OPTIONS, DELETE',
+}));
 
-// app.all('/*', (req: Request, res: Response, next: NextFunction) =>{
-//     console.log(req.path)
-//     const publicRoutes = ['/authentication', '/', '/users'];
-//     for (let i = 0; i < publicRoutes.length; i +=1) {
-//         if (req.path === publicRoutes[i]) {
-//             return next();
-//         }
-//     } 
-//     auth(req, res, next);
-// } )
+app.all('/*', (req: Request, res: Response, next: NextFunction) =>{
+    console.log(req.path)
+    const publicRoutes = ['/authentication', '/', '/users'];
+    for (let i = 0; i < publicRoutes.length; i +=1) {
+        if (req.path === publicRoutes[i]) {
+            return next();
+        }
+    } 
+    auth(req, res, next);
+} )
 
 app.use('/', routes);
 
